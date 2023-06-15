d663212017e497711e53dd9e15d817fb64e18566
Samuel Senerwa <samuelsenerwa@gmail.com>
2023-06-15 01:34:06 -0700

    fixing checker

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,13 @@
-#!/usr/bin/ruby
-# Increasing the number of traffic Nginx can handle
+# Increases the amount of traffic an Nginx server can handle.
 
-exec { 'fix-for-nginx':
-  command     => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path        => '/usr/local/bin:/bin',
-  refreshonly => true,
-}
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-# Restarting Nginx
+# Restart Nginx
 exec { 'nginx-restart':
-  command => 'nginx-restart',
-  path    => '/etc/init.d',
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
