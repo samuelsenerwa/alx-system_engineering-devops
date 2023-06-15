6ca11ed7765fe28d669a62ed466d6a091852d258
Samuel Senerwa <samuelsenerwa@gmail.com>
2023-06-15 01:12:12 -0700

    puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,13 @@
 # Increasing the number of traffic Nginx can handle
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => 'usr/local/bin/:bin/'
+  command     => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path        => 'usr/local/bin/:bin/',
+  refreshonly => true,
 } ->
 
 # Restarting Nginx
 exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d/',
 }
